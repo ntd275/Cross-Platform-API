ccc3e01212a17f2b4fe4c020426bc33a49ed55e1
NMHIEU2021999 <minhhieu20219992@gmail.com>
2021-11-14 12:22:44 +0700

    udpate

--- controllers/Searchs.js
+++ controllers/Searchs.js
@@ -1,6 +1,8 @@
 const ChatModel = require("../models/Chats");
-const FriendModel = require("../models/Friends")
+const FriendModel = require("../models/Friends");
+const Messages = require("../models/Messages");
 const MessagesModel = require("../models/Messages");
+const Users = require("../models/Users");
 const httpStatus = require("../utils/httpStatus");
 const searchController = {};
 
@@ -8,30 +10,103 @@ searchController.search = async (req, res, next) => {
     try {
         let key = req.params.key;
         userId = req.userId;
-        friendList = [];
-        peopleList = [];
-        messageList = [];
-        let chat = await ChatModel.findOne({
+        let friendList = [];
+        let peopleList = [];
+        let messageList = [];
+        let friends = await FriendModel.find({
             $and: [
-                { _id: req.params.chatId },
-                { members: req.userId }
+                {
+                    $or: [
+                        { sender: userId },
+                        { receiver: userId }
+                    ]
+                },
+                { status: "1" }
             ]
+
         }).populate('messsages');
-        if (chat !== null) {
-            let pivots = chat.pivots;
-            let curPivot = 0;
-            for(let i = 0; i< chat.members.length; i++){
-                if(chat.members[i] == req.userId){
-                    curPivot = pivots[i];
-                    break;
-                }
+
+        let friendIds = [];
+        for (let i = 0; i < friends.length; i++) {
+            if (friends[i].sender == userId) {
+                friendIds.push(friends[i].receiver);
+            } else {
+                friendIds.push(friends[i].sender);
             }
-            messsages = chat.messsages;
-            messsages.splice(0, curPivot);
-            return res.status(httpStatus.OK).json({
-                data: messsages
-            });
         }
+
+        friendList = UserModel.find({
+            $and: [
+                { _id: { $in: friendIds } },
+                {
+                    $or: [
+                        { username: { "$regex": key, "$options": "i" } },
+                        { phonenumber: { "$regex": key, "$options": "i" } }
+                    ]
+                }
+            ]
+        }).populate('avatar');
+
+        peopleList = UserModel.find({
+            $and: [
+                { _id: { $nin: friendIds } },
+                {
+                    $or: [
+                        { username: { "$regex": key, "$options": "i" } },
+                        { phonenumber: { "$regex": key, "$options": "i" } }
+                    ]
+                }
+            ]
+        }).populate('avatar');
+
+        let messages = MessagesModel.find({
+            $and: [
+                {
+                    $or: [
+                        { senderId: userId },
+                        { receiverId: userId }
+                    ]
+                },
+                { content: { "$regex": key, "$options": "i" } },
+            ]
+        }).populate({
+            path: 'senderId',
+            model: 'Users',
+            populate: {
+                path: 'avatar',
+                model: 'Documents'
+            }
+        }).populate({
+            path: 'receiverId',
+            model: 'Users',
+            populate: {
+                path: 'avatar',
+                model: 'Documents'
+            }
+        });
+
+        for(let i = 0; i< messages.length; i++){
+            let message = {};
+            message.content = messages[i].content;
+            message.time = messages[i].time;
+            message.chatId = messages[i].chatId;
+            if(messages[i].senderId._id == userId){
+                message["friend"] = messages[i].receiverId;
+            }else{
+                message["friend"] = messages[i].senderId;
+            }
+
+            messageList.push(message);
+        }
+
+        return res.status(httpStatus.OK).json({
+            data: {
+                friends: friendList,
+                people: peopleList,
+                messages: messageList
+            }
+        });
+
     } catch (e) {
         return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
             message: e.message
